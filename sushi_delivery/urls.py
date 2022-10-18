3b96b4d8435aa04691914a399105e751649ee478
ruslan-kornich <korny4rus143@gmail.com>
2022-10-18 17:48:56 +0300

    order processing

--- sushi_delivery/urls.py
+++ sushi_delivery/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('app.urls', namespace='shop')),
 ]
